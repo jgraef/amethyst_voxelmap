e43d9ce1763d905d2e0f38d71262f9f4610b9b12
Janosch Gräf <janosch.graef@gmail.com>
2021-06-30 17:13:56 +0200

    Fixed typo

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! Provides a `RenderPlugin` to render `VoxelMap` components. A `VoxelMap` is
 //! a volumetric 3D structure containing cubic voxels. It is analogue to
-//! [`amethyst_tilemap`](https://github.com/amethyst/amethyst/tree/main/amethyst_tiles)
+//! [`amethyst_tiles`](https://github.com/amethyst/amethyst/tree/main/amethyst_tiles)
 //! in three dimensions.
 //!
 //! ![Screenshot](/examples/screenshot.png)
